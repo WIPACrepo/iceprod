997d75b622c5ce68632646a6b697dd5d92f6aca6
David Schultz <davids24@gmail.com>
2016-06-09 14:02:31 -0500

    forgot indentation

--- iceprod/core/exe.py
+++ iceprod/core/exe.py
@@ -609,14 +609,14 @@ def run_module(cfg, env, module):
     if module['env_shell']:
         env_shell = module['env_shell'].split()
         if not os.path.exists(env_shell[0]):
-        # get script to run
-        c = dataclasses.Class()
-        c['src'] = env_shell[0]
-        c['name'] = os.path.basename(c['src'])
-        setupClass(env,c)
-        if c['name'] not in env['classes']:
-            raise Exception('Failed to install class %s'%c['name'])
-        env_shell[0] = [env['classes'][c['name']]]
+            # get script to run
+            c = dataclasses.Class()
+            c['src'] = env_shell[0]
+            c['name'] = os.path.basename(c['src'])
+            setupClass(env,c)
+            if c['name'] not in env['classes']:
+                raise Exception('Failed to install class %s'%c['name'])
+            env_shell[0] = [env['classes'][c['name']]]
         module['env_shell'] = env_shell
 
     logger.warn('running module \'%s\' with class %s',module['name'],
