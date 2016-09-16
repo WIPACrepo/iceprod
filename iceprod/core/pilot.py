a2228da645ff9c1a449ccb023db1f17303d84340
David Schultz <davids24@gmail.com>
2016-09-16 15:39:52 -0500

    reduce logging by getting the condition right

--- iceprod/core/pilot.py
+++ iceprod/core/pilot.py
@@ -141,7 +141,7 @@ class Pilot(object):
                     reason = ''
                     for r in used_resources:
                         overusage = used_resources[r]-task.resources[r]
-                        if overusage:
+                        if overusage > 0:
                             overusage_percent = used_resources[r]*1.0/task.resources[r]
                             if (r in Task_Resource_Overusage and
                                 (overusage_percent < Task_Resource_Overusage[r]['ignore'] or
