91d59cb4ae5ce0b678cdae30c1fa059e016136b8
David Schultz <davids24@gmail.com>
2016-10-31 13:08:16 -0500

    upgrade psutil

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ if setuptools is not None:
     # If setuptools is not available, you're on your own for dependencies.
     install_requires = ['certifi','tornado>=4.2', 'pyzmq', 'setproctitle',
                         'pyOpenSSL', 'pyasn1', 'jsonschema',
-                        'psutil==2.2.1', 'cryptography', 'requests',
+                        'psutil>=4.0.0', 'cryptography', 'requests',
                         'requests_toolbelt']
     extras_require = {
         'docs': ['sphinx'],
