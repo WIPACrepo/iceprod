1970123cc0fd8e6cc3e48addd3a641df3da8b6d6
David Delventhal <ddelventhal@icecube.wisc.edu>
2017-05-19 14:12:22 -0500

    Added get dataset completion call

--- iceprod/server/dbmethods/rpc.py
+++ iceprod/server/dbmethods/rpc.py
@@ -1171,3 +1171,14 @@ class rpc(_Methods_Base):
         ret = yield self.parent.db.query(sql, bindings)
         ret = [i[0] for i in ret]
         raise tornado.gen.Return(ret)
+
+    @tornado.gen.coroutine
+    def rpc_public_get_dataset_completion(self, dataset_id):
+        sql = 'SELECT COUNT(task.task_id) FROM task_rel JOIN task ON task_rel.task_rel_id = task.task_rel_id WHERE dataset_id = ?;'
+        bindings = (dataset_id,)
+        ret = yield self.parent.db.query(sql, bindings)
+        total = ret[0][0]
+        sql = 'SELECT COUNT(task.task_id) FROM task_rel JOIN task ON task_rel.task_rel_id = task.task_rel_id WHERE dataset_id = ? and status = "complete";'
+        ret = yield self.parent.db.query(sql, bindings)
+        done = ret[0][0]
+        raise tornado.gen.Return(done / float(total))
