6ef90908909cd3913dcdfd3f3f7de8732b1580dc
David Schultz <davids24@gmail.com>
2017-07-06 14:28:16 -0500

    fix getcwd

--- iceprod/core/i3exec.py
+++ iceprod/core/i3exec.py
@@ -237,7 +237,7 @@ def runner(config, url, debug=False, offline=False):
             resource_stop = False
             resources = iceprod.core.resources.Resources(debug=debug)
             resources.claim('a')
-            resources.register_process(psutil.Process(), os.get_cwd())
+            resources.register_process(psutil.Process(), os.getcwd())
             def track():
                 while not resource_stop:
                     resources.check_claims()
