7d8362293f0a42708bb84d4fcbd9d34e7011a06e
David Schultz <davids24@gmail.com>
2017-10-18 14:37:16 -0500

    make coverage.sh return the correct error code from testing

--- coverage.sh
+++ coverage.sh
@@ -6,8 +6,8 @@ export LD_LIBRARY_PATH=$PWD/resource_libs:$LD_LIBRARY_PATH
 rm -f .coverage*;
 $COVERAGE erase;
 $COVERAGE run --source iceprod,bin --parallel-mode --branch -m tests $@;
-#if [ $? = 0 ]; then
-    $COVERAGE combine;
-    echo generating html coverage in 'htmlcov';
-    $COVERAGE html -i;
-#fi
+ERR=$?
+$COVERAGE combine;
+echo generating html coverage in 'htmlcov';
+$COVERAGE html -i;
+exit $ERR
