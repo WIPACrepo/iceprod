af23bc6a1444c17e8e567754a9eaa14613f15ae2
David Schultz <davids24@gmail.com>
2017-10-19 17:05:05 -0500

    add requests-mock to requirements

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ if setuptools is not None:
                         'pyOpenSSL', 'pyasn1', 'jsonschema',
                         'psutil>=5.0.0', 'cryptography', 'requests',
                         'requests_toolbelt', 'requests-futures', 'statsd',
-                        'cachetools>=2.0.0', 'sphinx>=1.4', 'coverage', 'flexmock'
+                        'cachetools>=2.0.0', 'sphinx>=1.4',
+                        'coverage', 'flexmock', 'requests-mock',
                        ]
     if sys.version_info < (3, 2):
         install_requires.extend(['futures','subprocess32'])
