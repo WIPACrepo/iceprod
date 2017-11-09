a2482af131b0d07d4039b1dda8377d1e8ef56f80
David Schultz <davids24@gmail.com>
2017-11-08 18:07:34 -0600

    fix proxy

--- iceprod/server/globus.py
+++ iceprod/server/globus.py
@@ -88,4 +88,4 @@ class SiteGlobusProxy(object):
         """Get the proxy location"""
         FNULL = open(os.devnull, 'w')
         return subprocess.check_output(['grid-proxy-info','-path'],
-                                       stderr=FNULL).strip()
+                                       stderr=FNULL).decode('utf-8').strip()
