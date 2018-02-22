aa39768c51eaf77803af35691a594947af05d56d
David Schultz <davids24@gmail.com>
2018-02-22 15:06:09 -0600

    add /logs

---
+++ iceprod/server/rest/logs.py
@@ -0,0 +1,117 @@
+import logging
+import json
+import uuid
+
+import tornado.web
+import pymongo
+import motor
+
+from iceprod.server.rest import RESTHandler, RESTHandlerSetup, authorization, catch_error
+
+logger = logging.getLogger('rest.logs')
+
+def setup(config):
+    """
+    Setup method for Logs REST API.
+
+    Sets up any database connections or other prerequisites.
+
+    Args:
+        config (dict): an instance of :py:class:`iceprod.server.config`.
+
+    Returns:
+        list: Routes for logs, which can be passed to :py:class:`tornado.web.Application`.
+    """
+    cfg_auth = config.get('rest',{}).get('logs',{})
+    db_name = cfg_auth.get('database','mongodb://localhost:27017')
+
+    # add indexes
+    db = pymongo.MongoClient(db_name).auth
+    if 'log_id_index' not in db.logs.index_information():
+        db.logs.create_index('log_id', name='log_id_index', unique=True)
+
+    handler_cfg = RESTHandlerSetup(config)
+    handler_cfg.update({
+        'database': motor.motor_tornado.MotorClient(db_name).config,
+    })
+
+    return [
+        (r'/logs', MultiLogsHandler, handler_cfg),
+        (r'/logs/(?P<log_id>\w+)', LogsHandler, handler_cfg),
+        (r'/datasets/(?P<dataset_id>\w+)/logs/(?P<log_id>\w+)', DatasetLogsHandler, handler_cfg),
+    ]
+
+class BaseHandler(RESTHandler):
+    def initialize(self, database=None, **kwargs):
+        super(BaseHandler, self).initialize(**kwargs)
+        self.db = database
+
+class MultiLogsHandler(BaseHandler):
+    """
+    Handle logs requests.
+    """
+    @authorization(roles=['admin','pilot'])
+    async def post(self):
+        """
+        Create a log entry.
+
+        Body should contain the log message.
+
+        Returns:
+            dict: {'result': <log_id>}
+        """
+        data = {
+            'log_id': uuid.uuid1().hex,
+            'data': self.request.body,
+        }
+        ret = await self.db.logs.insert_one(data)
+        self.set_status(201)
+        self.write({'result': data['log_id']})
+        self.finish()
+
+class LogsHandler(BaseHandler):
+    """
+    Handle logs requests.
+    """
+    @authorization(roles=['admin'])
+    async def get(self, log_id):
+        """
+        Get a log entry.
+
+        Args:
+            log_id (str): the log id of the entry
+
+        Returns:
+            str: log entry
+        """
+        ret = await self.db.logs.find_one({'log_id':log_id},
+                projection={'_id':False, 'data':True})
+        if not ret:
+            self.send_error(404, reason="Log not found")
+        else:
+            self.write(ret['data'])
+            self.finish()
+
+class DatasetLogsHandler(BaseHandler):
+    """
+    Handle logs requests.
+    """
+    @authorization(roles=['admin'], attrs=['dataset_id:read'])
+    async def get(self, dataset_id, log_id):
+        """
+        Get a config.
+
+        Args:
+            dataset_id (str): the dataset id of the config
+            log_id (str): the log id of the entry
+
+        Returns:
+            dict: config
+        """
+        ret = await self.db.logs.find_one({'log_id':log_id},
+                projection={'_id':False, 'data':True})
+        if not ret:
+            self.send_error(404, reason="Log not found")
+        else:
+            self.write(ret['data'])
+            self.finish()
---
+++ tests/server/rest_logs_test.py
@@ -0,0 +1,125 @@
+"""
+Test script for REST/logs
+"""
+
+import logging
+logger = logging.getLogger('rest_logs_test')
+
+import os
+import sys
+import time
+import random
+import shutil
+import tempfile
+import unittest
+import subprocess
+import json
+from functools import partial
+from unittest.mock import patch, MagicMock
+
+from tests.util import unittest_reporter, glob_tests
+
+import ldap3
+import tornado.web
+import tornado.ioloop
+from tornado.httpclient import AsyncHTTPClient, HTTPError
+from tornado.testing import AsyncTestCase
+
+import iceprod.server.tornado
+import iceprod.server.rest.config
+from iceprod.server.auth import Auth
+
+class rest_logs_test(AsyncTestCase):
+    def setUp(self):
+        super(rest_logs_test,self).setUp()
+        self.test_dir = tempfile.mkdtemp(dir=os.getcwd())
+        def cleanup():
+            shutil.rmtree(self.test_dir)
+        self.addCleanup(cleanup)
+
+        try:
+            self.port = random.randint(10000,50000)
+            self.mongo_port = random.randint(10000,50000)
+            dbpath = os.path.join(self.test_dir,'db')
+            os.mkdir(dbpath)
+            dblog = os.path.join(dbpath,'logfile')
+
+            m = subprocess.Popen(['mongod', '--port', str(self.mongo_port),
+                                  '--dbpath', dbpath, '--smallfiles',
+                                  '--quiet', '--nounixsocket',
+                                  '--logpath', dblog])
+            self.addCleanup(partial(time.sleep, 0.05))
+            self.addCleanup(m.terminate)
+
+            config = {
+                'auth': {
+                    'secret': 'secret'
+                },
+                'rest': {
+                    'logs': {
+                        'database': 'mongodb://localhost:'+str(self.mongo_port),
+                    }
+                },
+            }
+            self.app = iceprod.server.tornado.setup_rest(config)
+            self.token = Auth('secret').create_token('foo', type='user', payload={'role':'admin'})
+        except Exception:
+            logger.info('failed setup', exc_info=True)
+
+
+    @unittest_reporter(name='REST POST   /logs')
+    def test_100_logs(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = 'foo bar baz'
+        r = yield client.fetch('http://localhost:%d/logs'%self.port,
+                method='POST', body=data,
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        log_id = ret['result']
+
+    @unittest_reporter(name='REST GET    /logs/<log_id>')
+    def test_110_logs(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = 'foo bar baz'
+        r = yield client.fetch('http://localhost:%d/logs'%self.port,
+                method='POST', body=data,
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        log_id = ret['result']
+
+        r = yield client.fetch('http://localhost:%d/logs/%s'%(self.port,log_id),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+        ret = r.body.decode('utf-8')
+        self.assertEqual(data, ret)
+
+    @unittest_reporter(name='REST GET    /datasets/<dataset_id>/logs/<log_id>')
+    def test_120_logs(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = 'foo bar baz'
+        r = yield client.fetch('http://localhost:%d/logs'%self.port,
+                method='POST', body=data,
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        log_id = ret['result']
+
+        r = yield client.fetch('http://localhost:%d/datasets/12345/logs/%s'%(self.port,log_id),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+        ret = r.body.decode('utf-8')
+        self.assertEqual(data, ret)
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    alltests = glob_tests(loader.getTestCaseNames(rest_logs_test))
+    suite.addTests(loader.loadTestsFromNames(alltests,rest_logs_test))
+    return suite
