571a7f8d7831716ac494f4ea0de00c0a4885cca6
David Schultz <davids24@gmail.com>
2018-02-27 15:22:02 -0600

    add rest api for pilots

---
+++ iceprod/server/rest/pilots.py
@@ -0,0 +1,168 @@
+import logging
+import json
+import uuid
+
+import tornado.web
+import pymongo
+import motor
+
+from iceprod.server.rest import RESTHandler, RESTHandlerSetup, authorization
+from iceprod.server.util import nowstr
+
+logger = logging.getLogger('rest.pilots')
+
+def setup(config):
+    """
+    Setup method for Pilots REST API.
+
+    Sets up any database connections or other prerequisites.
+
+    Args:
+        config (dict): an instance of :py:class:`iceprod.server.config`.
+
+    Returns:
+        list: Routes for logs, which can be passed to :py:class:`tornado.web.Application`.
+    """
+    cfg_auth = config.get('rest',{}).get('pilots',{})
+    db_name = cfg_auth.get('database','mongodb://localhost:27017')
+
+    # add indexes
+    db = pymongo.MongoClient(db_name).pilots
+    if 'pilot_id_index' not in db.pilots.index_information():
+        db.pilots.create_index('pilot_id', name='pilot_id_index', unique=True)
+
+    handler_cfg = RESTHandlerSetup(config)
+    handler_cfg.update({
+        'database': motor.motor_tornado.MotorClient(db_name).pilots,
+    })
+
+    return [
+        (r'/pilots', MultiPilotsHandler, handler_cfg),
+        (r'/pilots/(?P<pilot_id>\w+)', PilotsHandler, handler_cfg),
+    ]
+
+class BaseHandler(RESTHandler):
+    def initialize(self, database=None, **kwargs):
+        super(BaseHandler, self).initialize(**kwargs)
+        self.db = database
+
+class MultiPilotsHandler(BaseHandler):
+    """
+    Handle multi pilots requests.
+    """
+    @authorization(roles=['admin','client','system'])
+    async def get(self):
+        """
+        Get pilot entries.
+
+        Returns:
+            dict: {'uuid': {pilot_data}}
+        """
+        ret = await self.db.pilots.find(projection={'_id':False}).to_list(1000)
+        self.write({row['pilot_id']:row for row in ret})
+        self.finish()
+
+    @authorization(roles=['admin','client'])
+    async def post(self):
+        """
+        Create a pilot entry.
+
+        Body should contain the pilot data.
+
+        Returns:
+            dict: {'result': <pilot_id>}
+        """
+        data = json.loads(self.request.body)
+
+        # validate first
+        req_fields = {
+            'host': str,
+            'version': str, # iceprod version
+        }
+        for k in req_fields:
+            if k not in data:
+                raise tornado.web.HTTPError(400, reason='missing key: '+k)
+            if not isinstance(data[k], req_fields[k]):
+                r = 'key {} should be of type {}'.format(k, req_fields[k])
+                raise tornado.web.HTTPError(400, reason=r)
+
+        # set some fields
+        data['pilot_id'] = uuid.uuid1().hex
+        data['start_date'] = nowstr()
+        data['last_update'] = data['start_date']
+        if 'tasks' not in data:
+            data['tasks'] = []
+
+        ret = await self.db.pilots.insert_one(data)
+        self.set_status(201)
+        self.write({'result': data['pilot_id']})
+        self.finish()
+
+class PilotsHandler(BaseHandler):
+    """
+    Handle single pilot requests.
+    """
+    @authorization(roles=['admin','client','pilot'])
+    async def get(self, pilot_id):
+        """
+        Get a pilot entry.
+
+        Args:
+            pilot_id (str): the pilot id
+
+        Returns:
+            dict: pilot entry
+        """
+        ret = await self.db.pilots.find_one({'pilot_id':pilot_id},
+                projection={'_id':False})
+        if not ret:
+            self.send_error(404, reason="Pilot not found")
+        else:
+            self.write(ret)
+            self.finish
+
+    @authorization(roles=['admin','client','pilot'])
+    async def patch(self, pilot_id):
+        """
+        Update a pilot entry.
+
+        Body should contain the pilot data to update.  Note that this will
+        perform a merge (not replace).
+
+        Args:
+            pilot_id (str): the pilot id
+
+        Returns:
+            dict: updated pilot entry
+        """
+        data = json.loads(self.request.body)
+        if not data:
+            raise tornado.web.HTTPError(400, reason='Missing update data')
+        data['last_update'] = nowstr()
+
+        ret = await self.db.pilots.find_one_and_update({'pilot_id':pilot_id},
+                {'$set':data},
+                projection={'_id':False},
+                return_document=pymongo.ReturnDocument.AFTER)
+        if not ret:
+            self.send_error(404, reason="Pilot not found")
+        else:
+            self.write(ret)
+            self.finish()
+
+    @authorization(roles=['admin','client','pilot'])
+    async def delete(self, pilot_id):
+        """
+        Delete a pilot entry.
+
+        Args:
+            pilot_id (str): the pilot id
+
+        Returns:
+            dict: empty dict
+        """
+        ret = await self.db.pilots.delete_one({'pilot_id':pilot_id})
+        if (not ret) or (ret.deleted_count < 1):
+            self.send_error(404, reason="Pilot not found")
+        else:
+            self.write({})
---
+++ tests/server/rest_pilots_test.py
@@ -0,0 +1,190 @@
+"""
+Test script for REST/pilots
+"""
+
+import logging
+logger = logging.getLogger('rest_pilots_test')
+
+import os
+import sys
+import time
+import random
+import shutil
+import tempfile
+import unittest
+import subprocess
+import json
+from functools import partial
+from unittest.mock import patch, MagicMock
+
+from tests.util import unittest_reporter, glob_tests
+
+import ldap3
+import tornado.web
+import tornado.ioloop
+from tornado.httpclient import AsyncHTTPClient, HTTPError
+from tornado.testing import AsyncTestCase
+
+import iceprod.server.tornado
+import iceprod.server.rest.config
+from iceprod.server.auth import Auth
+
+class rest_pilots_test(AsyncTestCase):
+    def setUp(self):
+        super(rest_pilots_test,self).setUp()
+        self.test_dir = tempfile.mkdtemp(dir=os.getcwd())
+        def cleanup():
+            shutil.rmtree(self.test_dir)
+        self.addCleanup(cleanup)
+
+        try:
+            self.port = random.randint(10000,50000)
+            self.mongo_port = random.randint(10000,50000)
+            dbpath = os.path.join(self.test_dir,'db')
+            os.mkdir(dbpath)
+            dblog = os.path.join(dbpath,'logfile')
+
+            m = subprocess.Popen(['mongod', '--port', str(self.mongo_port),
+                                  '--dbpath', dbpath, '--smallfiles',
+                                  '--quiet', '--nounixsocket',
+                                  '--logpath', dblog])
+            self.addCleanup(partial(time.sleep, 0.05))
+            self.addCleanup(m.terminate)
+
+            config = {
+                'auth': {
+                    'secret': 'secret'
+                },
+                'rest': {
+                    'pilots': {
+                        'database': 'mongodb://localhost:'+str(self.mongo_port),
+                    }
+                },
+            }
+            self.app = iceprod.server.tornado.setup_rest(config)
+            self.token = Auth('secret').create_token('foo', type='user', payload={'role':'admin','username':'admin'})
+        except Exception:
+            logger.info('failed setup', exc_info=True)
+
+    @unittest_reporter(name='REST GET    /pilots')
+    def test_100_pilots(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        r = yield client.fetch('http://localhost:%d/pilots'%self.port,
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+        ret = json.loads(r.body)
+        self.assertEqual(ret, {})
+
+    @unittest_reporter(name='REST POST   /pilots')
+    def test_105_pilots(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = {
+            'host': 'foo.bar.baz',
+            'version': '1.2.3',
+        }
+        r = yield client.fetch('http://localhost:%d/pilots'%self.port,
+                method='POST', body=json.dumps(data),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        pilot_id = ret['result']
+
+        r = yield client.fetch('http://localhost:%d/pilots'%self.port,
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+        ret = json.loads(r.body)
+        self.assertIn(pilot_id, ret)
+        for k in data:
+            self.assertIn(k, ret[pilot_id])
+            self.assertEqual(data[k], ret[pilot_id][k])
+
+    @unittest_reporter(name='REST GET    /pilots/<pilot_id>')
+    def test_110_pilots(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = {
+            'host': 'foo.bar.baz',
+            'version': '1.2.3',
+        }
+        r = yield client.fetch('http://localhost:%d/pilots'%self.port,
+                method='POST', body=json.dumps(data),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        pilot_id = ret['result']
+
+        r = yield client.fetch('http://localhost:%d/pilots/%s'%(self.port,pilot_id),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+        ret = json.loads(r.body)
+        for k in data:
+            self.assertIn(k, ret)
+            self.assertEqual(data[k], ret[k])
+        self.assertIn('tasks', ret)
+        self.assertEqual(ret['tasks'], [])
+
+    @unittest_reporter(name='REST PATCH  /pilots/<pilot_id>')
+    def test_120_pilots(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = {
+            'host': 'foo.bar.baz',
+            'version': '1.2.3',
+        }
+        r = yield client.fetch('http://localhost:%d/pilots'%self.port,
+                method='POST', body=json.dumps(data),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        pilot_id = ret['result']
+
+        new_data = {
+            'queues': {'foo': 'HTCondor', 'bar': 'HTCondor'},
+            'version': '1.2.8',
+            'tasks': ['baz'],
+        }
+        r = yield client.fetch('http://localhost:%d/pilots/%s'%(self.port,pilot_id),
+                method='PATCH', body=json.dumps(new_data),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+        ret = json.loads(r.body)
+        for k in new_data:
+            self.assertIn(k, ret)
+            self.assertEqual(new_data[k], ret[k])
+
+    @unittest_reporter(name='REST DELETE /pilots/<pilot_id>')
+    def test_130_pilots(self):
+        iceprod.server.tornado.startup(self.app, port=self.port, io_loop=self.io_loop)
+
+        client = AsyncHTTPClient(self.io_loop)
+        data = {
+            'host': 'foo.bar.baz',
+            'version': '1.2.3',
+        }
+        r = yield client.fetch('http://localhost:%d/pilots'%self.port,
+                method='POST', body=json.dumps(data),
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 201)
+        ret = json.loads(r.body)
+        pilot_id = ret['result']
+
+        r = yield client.fetch('http://localhost:%d/pilots/%s'%(self.port,pilot_id),
+                method='DELETE',
+                headers={'Authorization': b'bearer '+self.token})
+        self.assertEqual(r.code, 200)
+
+        with self.assertRaises(Exception):
+            r = yield client.fetch('http://localhost:%d/pilots/%s'%(self.port,pilot_id),
+                    headers={'Authorization': b'bearer '+self.token})
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    alltests = glob_tests(loader.getTestCaseNames(rest_pilots_test))
+    suite.addTests(loader.loadTestsFromNames(alltests,rest_pilots_test))
+    return suite
