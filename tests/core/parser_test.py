790e622ba9eab03f99e0456ff1ae53400af1564c
David Schultz <davids24@gmail.com>
2018-04-24 17:56:43 -0500

    parse multi-nested dict/list with recursion

--- tests/core/parser_test.py
+++ tests/core/parser_test.py
@@ -672,6 +672,19 @@ class parser_test(unittest.TestCase):
             self.assertEqual(ret,i+1)
 
     @unittest_reporter
+    def test_34_parse_dict_array_notation(self):
+        """Test parsing dict+array hidden nested notation. A bug found during prod."""
+        job = dataclasses.Job()
+        job['steering'] = dataclasses.Steering()
+        job['steering']['parameters']['array'] = ["foo", "bar", "baz"]
+        job['steering']['parameters']['dict'] = {"foo":1, "bar":2, "baz":3}
+        job['steering']['parameters']['choose'] = '$steering(array)[0]'
+        p = parser.ExpParser()
+
+        ret = p.parse("$steering(dict)[$steering(choose)]",job=job)
+        self.assertEqual(ret,1)
+
+    @unittest_reporter
     def test_100_scanner(self):
         ret = list(parser.scanner('$foo(bar)'))
         expected = [('starter','$'),('word','foo'),('scopeL','('),('word','bar'),('scopeR',')')]
