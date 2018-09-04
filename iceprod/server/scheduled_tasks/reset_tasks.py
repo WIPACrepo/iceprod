9abe2bc2b2597ef174a7691ea71085c738b85a89
David Schultz <davids24@gmail.com>
2018-09-04 16:13:58 -0500

    add a scheduled task for resetting tasks back to a good status

---
+++ iceprod/server/scheduled_tasks/reset_tasks.py
@@ -0,0 +1,75 @@
+"""
+Move tasks from reset to waiting/failed/suspended.
+
+Initial delay: rand(5 minutes)
+Periodic delay: 20 minutes
+"""
+
+import logging
+import random
+import time
+
+from tornado.ioloop import IOLoop
+
+logger = logging.getLogger('reset_tasks')
+
+def reset_tasks(module):
+    """
+    Initial entrypoint.
+
+    Args:
+        module (:py:class:`iceprod.server.modules.schedule`): schedule module
+    """
+    # initial delay
+    IOLoop.current().call_later(random.randint(60,60*5), run, module.rest_client)
+
+async def run(rest_client, debug=False):
+    """
+    Actual runtime / loop.
+
+    Args:
+        rest_client (:py:class:`iceprod.core.rest_client.Client`): rest client
+        debug (bool): debug flag to propagate exceptions
+    """
+    start_time = time.time()
+
+    try:
+        datasets = await rest_client.request('GET', '/dataset_summaries/status')
+        dataset_ids = []
+        if 'processing' in datasets:
+            dataset_ids.extend(datasets['processing'])
+        if 'truncated' in datasets:
+            dataset_ids.extend(datasets['truncated'])
+        for dataset_id in dataset_ids:
+            try:
+                dataset = await rest_client.request('GET', '/datasets/{}'.format(dataset_id))
+                tasks = await rest_client.request('GET', '/datasets/{}/task_summaries/status'.format(dataset_id))
+                if 'reset' in tasks:
+                    logger.info('dataset %s reset tasks: %s', dataset_id, tasks['reset'])
+                    for task_id in tasks['reset']:
+                        try:
+                            task = await rest_client.request('GET', '/tasks/{}'.format(task_id))
+                            status = 'waiting'
+                            if dataset['debug']:
+                                status = 'suspended'
+                            elif 'failures' in task and task['failures'] > 10:
+                                status = 'failed'
+                            args = {'status': status}
+                            await rest_client.request('PUT', '/datasets/{}/tasks/{}/status'.format(dataset_id,task_id), args)
+                        except Exception:
+                            logger.error('error resetting task %s', task_id, exc_info=True)
+                            if debug:
+                                raise
+            except Exception:
+                logger.error('error resetting tasks in dataset %s', dataset_id, exc_info=True)
+                if debug:
+                    raise
+    except Exception:
+        logger.error('error resetting non-active tasks', exc_info=True)
+        if debug:
+            raise
+
+    # run again after 20 minute delay
+    stop_time = time.time()
+    delay = max(60*20 - (stop_time-start_time), 60)
+    IOLoop.current().call_later(delay, run, rest_client)
---
+++ tests/server/scheduled_tasks/reset_tasks_test.py
@@ -0,0 +1,121 @@
+"""
+Test script for scheduled_tasks/reset_tasks
+"""
+
+import logging
+logger = logging.getLogger('scheduled_tasks_reset_tasks_test')
+
+import os
+import sys
+import shutil
+import tempfile
+import unittest
+from functools import partial
+from unittest.mock import patch, MagicMock
+
+from tornado.testing import AsyncTestCase
+
+from tests.util import unittest_reporter, glob_tests
+
+from iceprod.core import rest_client
+from iceprod.server.modules.schedule import schedule
+from iceprod.server.scheduled_tasks import reset_tasks
+
+class reset_tasks_test(AsyncTestCase):
+    def setUp(self):
+        super(reset_tasks_test,self).setUp()
+        self.test_dir = tempfile.mkdtemp(dir=os.getcwd())
+        def cleanup():
+            shutil.rmtree(self.test_dir)
+        self.addCleanup(cleanup)
+
+        self.cfg = {
+            'queue':{
+                'init_queue_interval':0.1,
+                'submit_dir':self.test_dir,
+                '*':{'type':'Test1','description':'d'},
+            },
+            'master':{
+                'url':False,
+            },
+            'site_id':'abcd',
+        }
+
+    @unittest_reporter
+    def test_100_reset_tasks_test(self):
+        s = schedule(self.cfg,None,None,None)
+        reset_tasks.reset_tasks(s)
+
+    @unittest_reporter
+    async def test_200_run(self):
+        rc = MagicMock(spec=rest_client.Client)
+        pilots = {}
+        dataset_summaries = {'processing':['foo']}
+        tasks = {}
+        task = {}
+        async def client(method, url, args=None):
+            logger.info('REST: %s, %s', method, url)
+            if url.startswith('/dataset_summaries'):
+                return dataset_summaries
+            elif url.startswith('/datasets/foo/task_summaries'):
+                return tasks
+            elif url == '/datasets/foo':
+                return {'debug':False}
+            elif url == '/tasks/bar':
+                return task
+            elif url == '/datasets/foo/tasks/bar/status' and method == 'PUT':
+                client.called = True
+                return {}
+            else:
+                raise Exception()
+        client.called = False
+        rc.request = client
+
+        await reset_tasks.run(rc, debug=True)
+        self.assertFalse(client.called)
+
+        tasks['reset'] = ['bar']
+        await reset_tasks.run(rc, debug=True)
+        self.assertTrue(client.called)
+
+        client.called = False
+        del dataset_summaries['processing']
+        dataset_summaries['truncated'] = ['foo']
+        await reset_tasks.run(rc, debug=True)
+        self.assertTrue(client.called)
+
+
+    @unittest_reporter(name='run() - error')
+    async def test_201_run(self):
+        rc = MagicMock(spec=rest_client.Client)
+        pilots = {'a':{}}
+        # try tasks error
+        async def client(method, url, args=None):
+            logger.info('REST: %s, %s', method, url)
+            if url.startswith('/dataset_summaries'):
+                return {'processing':['foo']}
+            else:
+                raise Exception()
+        rc.request = client
+        with self.assertRaises(Exception):
+            await reset_tasks.run(rc, debug=True)
+
+        # check it normally hides the error
+        await reset_tasks.run(rc, debug=False)
+
+        # try dataset level error
+        async def client(method, url, args=None):
+            raise Exception()
+        rc.request = client
+        with self.assertRaises(Exception):
+            await reset_tasks.run(rc, debug=True)
+
+        # check it normally hides the error
+        await reset_tasks.run(rc, debug=False)
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    alltests = glob_tests(loader.getTestCaseNames(reset_tasks_test))
+    suite.addTests(loader.loadTestsFromNames(alltests,reset_tasks_test))
+    return suite
