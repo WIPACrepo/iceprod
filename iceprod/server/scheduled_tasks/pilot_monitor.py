44d25a1cef4bc248b8f5846f5ebf5b627c5d51d6
David Schultz <davids24@gmail.com>
2018-10-24 15:03:46 -0500

    only check resources on pilots that have them

--- iceprod/server/scheduled_tasks/pilot_monitor.py
+++ iceprod/server/scheduled_tasks/pilot_monitor.py
@@ -46,8 +46,9 @@ async def run(rest_client, statsd, debug=False):
         for pilot in pilots.values():
             for n in ('available','claimed'):
                 n2 = 'resources_'+n
-                for t in Resources.defaults:
-                    res[n][t] += pilot[n2][t] if t in pilot[n2] and pilot[n2][t] > 0 else 0
+                if n2 in pilot:
+                    for t in Resources.defaults:
+                        res[n][t] += pilot[n2][t] if t in pilot[n2] and pilot[n2][t] > 0 else 0
             count += len(pilot['tasks'])
         for n in res:
             for t in res[n]:
