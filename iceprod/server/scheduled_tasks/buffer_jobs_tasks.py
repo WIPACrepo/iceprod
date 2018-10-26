350def8f3ac5c05c011901c3f96f21407d775d0d
David Schultz <davids24@gmail.com>
2018-10-26 11:58:34 -0500

    fix buffering of jobs to do better at fancy requirements and config

--- iceprod/server/scheduled_tasks/buffer_jobs_tasks.py
+++ iceprod/server/scheduled_tasks/buffer_jobs_tasks.py
@@ -15,6 +15,7 @@ import time
 
 from tornado.ioloop import IOLoop
 
+from iceprod.core.parser import ExpParser
 from iceprod.core.resources import Resources
 
 logger = logging.getLogger('buffer_jobs_tasks')
@@ -50,11 +51,11 @@ async def run(rest_client, debug=False):
                     jobs_to_buffer = min(10000, dataset['jobs_submitted'] - len(jobs))
                     if jobs_to_buffer > 0:
                         config = await rest_client.request('GET', '/config/{}'.format(dataset_id))
+                        parser = ExpParser()
                         task_names = [task['name'] if task['name'] else str(i) for i,task in enumerate(config['tasks'])]
                         job_index = max(jobs[i]['job_index'] for i in jobs)+1 if jobs else 0
                         for i in range(jobs_to_buffer):
                             # buffer job
-                            job_index += 1
                             args = {'dataset_id': dataset_id, 'job_index': job_index}
                             job_id = await rest_client.request('POST', '/jobs', args)
                             # buffer tasks
@@ -62,16 +63,23 @@ async def run(rest_client, debug=False):
                             for task_index,name in enumerate(task_names):
                                 depends = await get_depends(rest_client, config, job_index,
                                                             task_index, task_ids)
+                                config['options']['job'] = job_index
+                                config['options']['task'] = task_index
+                                config['options']['dataset'] = dataset['dataset']
+                                config['options']['jobs_submitted'] = dataset['jobs_submitted']
+                                config['options']['tasks_submitted'] = dataset['tasks_submitted']
+                                config['options']['debug'] = dataset['debug']
                                 args = {
                                     'dataset_id': dataset_id,
                                     'job_id': job_id['result'],
                                     'task_index': task_index,
                                     'name': name,
                                     'depends': depends,
-                                    'requirements': get_reqs(config, task_index),
+                                    'requirements': get_reqs(config, task_index, parser),
                                 }
                                 task_id = await rest_client.request('POST', '/tasks', args)
                                 task_ids.append(task_id['result'])
+                            job_index += 1
             except Exception:
                 logger.error('error buffering dataset %s', dataset_id, exc_info=True)
                 if debug:
@@ -82,19 +90,25 @@ async def run(rest_client, debug=False):
     delay = max(60*10 - (stop_time-start_time), 60)
     IOLoop.current().call_later(delay, run, rest_client)
 
-def get_reqs(config, index):
+def get_reqs(config, task_index, parser):
     """
     Get requirements for a task.
 
     Args:
         config (:py:class:`iceprod.core.dataclasses.Job`): dataset config
-        index (int): task index
+        task_index (int): task index
+        parser (:py:class:`iceprod.core.parser.ExpParser`): parser
 
     Returns:
         dict: task requirements
     """
-    task = config['tasks'][index]
+    task = config['tasks'][task_index]
     req = task['requirements'].copy()
+    for k in req:
+        if isinstance(req[k], list):
+            req[k] = [parser.parse(val,config) for val in req[k]]
+        else:
+            req[k] = parser.parse(req[k],config)
     for k in Resources.defaults:
         if k not in req or not req[k]:
             req[k] = Resources.defaults[k]
@@ -121,30 +135,38 @@ async def get_depends(rest_client, config, job_index, task_index, task_ids):
     ret = []
     for dep in task['depends']:
         if dep in all_tasks:
+            logging.debug('dep %r in all_tasks', dep)
             dep_index = all_tasks[dep]
             if dep_index >= task_index:
                 raise Exception("bad depends: %r. can't depend on ourself or a greater index", dep)
             ret.append(task_ids[dep_index])
         elif isinstance(dep, int) and dep < len(all_tasks):
+            logging.debug('dep %r is int', dep)
             dep_index = dep
             if dep_index >= task_index:
                 raise Exception("bad depends: %r. can't depend on ourself or a greater index", dep)
             ret.append(task_ids[dep_index])
         elif isinstance(dep,str) and ':' in dep:
             # try for a task in another dataset
+            logging.debug('dep %r in another dataset', dep)
             dataset_id, dep = dep.split(':',1)
             try:
-                tasks = await rest_client.request('GET', '/datasets/{}/tasks?keys=task_id|name|task_index&job_index={}'.format(dataset_id,job_index))
+                #tasks = await rest_client.request('GET', '/datasets/{}/tasks?keys=task_id|name|task_index&job_index={}'.format(dataset_id,job_index))
+                tasks = await rest_client.request('GET', '/datasets/{}/tasks?keys=task_id|name|task_index'.format(dataset_id))
                 for task in tasks.values():
                     if dep == task['name'] or dep == str(task['task_index']):
-                        ret.append(task['task_id'])
-                        break
+                        job = await rest_client.request('GET', '/jobs/{}'.format(task['job_id']))
+                        logging.info('ext job_index=%r, my job_index=%r', job['job_index'], job_index)
+                        if job['job_index'] == job_index:
+                            ret.append(task['task_id'])
+                            break
                 else:
                     raise Exception()
             except Exception:
                 raise Exception('bad depends: %r', dep)
         else:
             # try for a raw task_id
+            logging.debug('dep %r is a raw id', dep)
             try:
                 task = await rest_client.request('GET', '/tasks/{}'.format(dep))
                 ret.append(task['task_id'])
--- tests/server/scheduled_tasks/buffer_jobs_tasks_test.py
+++ tests/server/scheduled_tasks/buffer_jobs_tasks_test.py
@@ -58,22 +58,22 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':[]},
                     {'name':'b','requirements':{'memory':4.5},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -82,8 +82,8 @@ class buffer_jobs_tasks_test(AsyncTestCase):
         self.assertTrue(client.called)
         self.assertTrue(len(jobs) == 2)
         self.assertTrue(len(tasks) == 4)
-        self.assertTrue([0,0,1,1], [t['job_id'] for t in tasks])
-        self.assertTrue([[],[0],[],[2]], [t['depends'] for t in tasks])
+        self.assertEqual([0,0,1,1], [t['job_id'] for t in tasks])
+        self.assertEqual([[],[0],[],[2]], [t['depends'] for t in tasks])
         
         job_ids = list(range(2))
         task_ids = list(range(4))
@@ -94,22 +94,22 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':[]},
                     {'name':'b','requirements':{},'depends':[0]},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -118,8 +118,8 @@ class buffer_jobs_tasks_test(AsyncTestCase):
         self.assertTrue(client.called)
         self.assertTrue(len(jobs) == 2)
         self.assertTrue(len(tasks) == 4)
-        self.assertTrue([0,0,1,1], [t['job_id'] for t in tasks])
-        self.assertTrue([[],[0],[],[2]], [t['depends'] for t in tasks])
+        self.assertEqual([0,0,1,1], [t['job_id'] for t in tasks])
+        self.assertEqual([[],[0],[],[2]], [t['depends'] for t in tasks])
         
         job_ids = list(range(1,2))
         task_ids = list(range(2,4))
@@ -130,22 +130,22 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {0:{'job_index':0}}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':[]},
                     {'name':'b','requirements':{},'depends':[0]},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -154,8 +154,8 @@ class buffer_jobs_tasks_test(AsyncTestCase):
         self.assertTrue(client.called)
         self.assertTrue(len(jobs) == 1)
         self.assertTrue(len(tasks) == 2)
-        self.assertTrue([1,1], [t['job_id'] for t in tasks])
-        self.assertTrue([[],[2]], [t['depends'] for t in tasks])
+        self.assertEqual([1,1], [t['job_id'] for t in tasks])
+        self.assertEqual([[],[2]], [t['depends'] for t in tasks])
 
     @unittest_reporter(name='run() - ext dep')
     async def test_201_run(self):
@@ -169,29 +169,33 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/datasets/bar/tasks'):
                 return {
-                    0:{'task_id':0,'name':'generate','task_index':0},
-                    1:{'task_id':1,'name':'filter','task_index':1},
-                    2:{'task_id':2,'name':'generate','task_index':2},
-                    3:{'task_id':3,'name':'filter','task_index':3},
+                    0:{'task_id':0,'job_id':0,'name':'generate','task_index':0},
+                    1:{'task_id':1,'job_id':0,'name':'filter','task_index':1},
+                    2:{'task_id':2,'job_id':1,'name':'generate','task_index':0},
+                    3:{'task_id':3,'job_id':1,'name':'filter','task_index':1},
                 }
+            elif url == '/jobs/0':
+                return {'job_id':0,'job_index':0}
+            elif url == '/jobs/1':
+                return {'job_id':1,'job_index':1}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':['bar:1']},
                     {'name':'b','requirements':{},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -200,8 +204,8 @@ class buffer_jobs_tasks_test(AsyncTestCase):
         self.assertTrue(client.called)
         self.assertTrue(len(jobs) == 2)
         self.assertTrue(len(tasks) == 4)
-        self.assertTrue([2,2,3,3], [t['job_id'] for t in tasks])
-        self.assertTrue([[1],[4],[3],[6]], [t['depends'] for t in tasks])
+        self.assertEqual([2,2,3,3], [t['job_id'] for t in tasks])
+        self.assertEqual([[1],[4],[3],[6]], [t['depends'] for t in tasks])
 
         job_ids = list(range(2,4))
         task_ids = list(range(4,8))
@@ -212,7 +216,7 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
@@ -220,16 +224,16 @@ class buffer_jobs_tasks_test(AsyncTestCase):
             elif url == '/tasks/3':
                 return {'task_id':3,'name':'filter','task_index':3}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':[3]},
                     {'name':'b','requirements':{},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -238,8 +242,8 @@ class buffer_jobs_tasks_test(AsyncTestCase):
         self.assertTrue(client.called)
         self.assertTrue(len(jobs) == 2)
         self.assertTrue(len(tasks) == 4)
-        self.assertTrue([2,2,3,3], [t['job_id'] for t in tasks])
-        self.assertTrue([[1],[4],[3],[6]], [t['depends'] for t in tasks])
+        self.assertEqual([2,2,3,3], [t['job_id'] for t in tasks])
+        self.assertEqual([[3],[4],[3],[6]], [t['depends'] for t in tasks])
 
     @unittest_reporter(name='run() - dep err')
     async def test_202_run(self):
@@ -254,22 +258,22 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':['b']},
                     {'name':'b','requirements':{},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -287,22 +291,22 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':[0]},
                     {'name':'b','requirements':{},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -320,22 +324,22 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'jobs_submitted':2, 'tasks_submitted':2}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':['lalala']},
                     {'name':'b','requirements':{},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -353,7 +357,7 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url == '/datasets/foo/jobs':
                 return {}
             elif url.startswith('/datasets/foo/task_counts'):
@@ -366,16 +370,16 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                     3:{'task_id':3,'name':'filter','task_index':3},
                 }
             elif url.startswith('/config'):
-                return {'tasks': [
+                return {'options':{},'tasks': [
                     {'name':'a','requirements':{},'depends':['bar:lalala']},
                     {'name':'b','requirements':{},'depends':['a']},
                 ]}
             elif url == '/jobs' and method == 'POST':
                 jobs.append(args)
-                return {'result': job_ids.pop()}
+                return {'result': job_ids.pop(0)}
             elif url == '/tasks' and method == 'POST':
                 tasks.append(args)
-                return {'result': task_ids.pop()}
+                return {'result': task_ids.pop(0)}
             else:
                 raise Exception()
         client.called = False
@@ -383,8 +387,58 @@ class buffer_jobs_tasks_test(AsyncTestCase):
         with self.assertRaises(Exception):
             await buffer_jobs_tasks.run(rc, debug=True)
 
+    @unittest_reporter(name='run() - req uses config')
+    async def test_205_run(self):
+        rc = MagicMock(spec=rest_client.Client)
+        job_ids = list(range(2,4))
+        task_ids = list(range(4,8))
+        jobs = []
+        tasks = []
+        async def client(method, url, args=None):
+            if url.startswith('/dataset_summaries'):
+                return {'processing':['foo']}
+            elif url == '/datasets/foo':
+                client.called = True
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
+            elif url == '/datasets/foo/jobs':
+                return {}
+            elif url.startswith('/datasets/foo/task_counts'):
+                return {}
+            elif url.startswith('/datasets/bar/tasks'):
+                return {
+                    0:{'task_id':0,'name':'generate','task_index':0},
+                    1:{'task_id':1,'name':'filter','task_index':1},
+                    2:{'task_id':2,'name':'generate','task_index':2},
+                    3:{'task_id':3,'name':'filter','task_index':3},
+                }
+            elif url.startswith('/config'):
+                return {'steering':{
+                    'parameters': {'test':'$eval(3+2.4)'}
+                },'options':{},
+                'tasks': [
+                    {'name':'a','requirements':{'cpu':'$eval(1+1)'},'depends':[]},
+                    {'name':'b','requirements':{'memory':'$eval($steering(test)+$(job))'},'depends':['a']},
+                ]}
+            elif url == '/jobs' and method == 'POST':
+                jobs.append(args)
+                return {'result': job_ids.pop(0)}
+            elif url == '/tasks' and method == 'POST':
+                tasks.append(args)
+                return {'result': task_ids.pop(0)}
+            else:
+                raise Exception()
+        client.called = False
+        rc.request = client
+        await buffer_jobs_tasks.run(rc, debug=True)
+        self.assertTrue(client.called)
+        self.assertTrue(len(jobs) == 2)
+        self.assertTrue(len(tasks) == 4)
+        self.assertEqual([2,2,3,3], [t['job_id'] for t in tasks])
+        self.assertEqual([2,1,2,1], [t['requirements']['cpu'] for t in tasks])
+        self.assertEqual([1.0,5.4,1.0,6.4], [t['requirements']['memory'] for t in tasks])
+
     @unittest_reporter(name='run() - no buffer')
-    async def test_203_run(self):
+    async def test_210_run(self):
         rc = MagicMock(spec=rest_client.Client)
         job_ids = list(range(2))
         task_ids = list(range(4))
@@ -395,9 +449,9 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url.startswith('/datasets/foo/task_counts'):
-                return {'waiting':3000}
+                return {'waiting':30000}
             else:
                 raise Exception()
         client.called = False
@@ -412,7 +466,7 @@ class buffer_jobs_tasks_test(AsyncTestCase):
                 return {'processing':['foo']}
             elif url == '/datasets/foo':
                 client.called = True
-                return {'jobs_to_queue':2, 'tasks_to_queue':2}
+                return {'dataset':1,'jobs_submitted':2, 'tasks_submitted':2,'debug':True}
             elif url.startswith('/datasets/foo/task_counts'):
                 return {}
             elif url == '/datasets/foo/jobs':
