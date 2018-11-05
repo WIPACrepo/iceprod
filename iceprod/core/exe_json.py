c062486bc6e15bcd950be4145ca0a1b2a6c11f3a
David Schultz <davids24@gmail.com>
2018-11-05 09:43:11 -0600

    Revert "fix time mismatch on client/server"
    This reverts commit 7b4ca19a487675a4386909f82092da269a21c79c.

--- iceprod/core/exe_json.py
+++ iceprod/core/exe_json.py
@@ -160,6 +160,8 @@ class ServerComms:
                               iceprod_stats)
 
         data = {}
+        if t:
+            data['time_used'] =  t/3600.
         await self.rest.request('POST', '/tasks/{}/task_actions/complete'.format(task_id), data)
 
     async def still_running(self, task_id):
@@ -216,6 +218,8 @@ class ServerComms:
             logging.warning('failed to post task_stats for %r', task_id, exc_info=True)
 
         data = {}
+        if t:
+            data['time_used'] =  t/3600.
         await self.rest.request('POST', '/tasks/{}/task_actions/reset'.format(task_id), data)
 
     async def task_kill(self, task_id, dataset_id=None, resources=None, reason=None, message=None):
@@ -258,7 +262,7 @@ class ServerComms:
 
         data = {}
         if resources and 'time' in resources and resources['time']:
-            data['time_used'] =  resources['time']*3600.
+            data['time_used'] =  resources['time']
         await self.rest.request('POST', '/tasks/{}/task_actions/reset'.format(task_id), data)
 
         data = {'name': 'stdlog', 'task_id': task_id}
@@ -363,7 +367,7 @@ class ServerComms:
 
         data = {}
         if resources and 'time' in resources and resources['time']:
-            data['time_used'] =  resources['time']*3600.
+            data['time_used'] =  resources['time']
         self.rest.request_seq('POST', '/tasks/{}/task_actions/reset'.format(task_id), data)
 
         data = {'name': 'stdlog', 'task_id': task_id}
