acb38616b0eb17fbda70f879f971d3f2054c0eb1
David Schultz <davids24@gmail.com>
2019-01-27 00:13:19 -0600

    add new scheduled task for cleaning up bad pilots

---
+++ iceprod/server/scheduled_tasks/pilot_cleanup.py
@@ -0,0 +1,58 @@
+"""
+Clean up the pilots.
+
+Initial delay: rand(60 minute)
+Periodic delay: 60 minutes
+"""
+
+import logging
+import random
+import time
+from datetime import datetime, timedelta
+
+from tornado.ioloop import IOLoop
+
+from iceprod.server.util import str2datetime
+
+logger = logging.getLogger('pilot_monitor')
+
+def pilot_cleanup(module):
+    """
+    Initial entrypoint.
+
+    Args:
+        module (:py:class:`iceprod.server.modules.schedule`): schedule module
+    """
+    # initial delay
+    IOLoop.current().call_later(random.randint(60,60*60), run,
+                                module.rest_client)
+
+async def run(rest_client, debug=False):
+    """
+    Actual runtime / loop.
+
+    Args:
+        rest_client (:py:class:`iceprod.core.rest_client.Client`): rest client
+        debug (bool): debug flag to propagate exceptions
+    """
+    start_time = time.time()
+    time_limit = datetime.utcnow() - timedelta(days=7)
+    async def reset_pilot(pilot_id):
+        await rest_client.request('DELETE', '/pilots/{}'.format(pilot_id))
+    try:
+        pilots = await rest_client.request('GET', '/pilots')
+        for pilot in pilots.values():
+            if 'grid_queue_id' not in pilot or not pilot['grid_queue_id']:
+                await reset_pilot(pilot['pilot_id'])
+            elif 'last_update' not in pilot or str2datetime(pilot['last_update']) < time_limit:
+                await reset_pilot(pilot['pilot_id'])
+                
+    except Exception:
+        logger.error('error cleaning pilots', exc_info=True)
+        if debug:
+            raise
+
+    # run again after 5 minute delay
+    stop_time = time.time()
+    delay = max(60*60 - (stop_time-start_time), 60)
+    IOLoop.current().call_later(delay, run, rest_client)
--- tests/server/scheduled_tasks/pilot_cleanup_test.py
+++ tests/server/scheduled_tasks/pilot_cleanup_test.py
@@ -1,9 +1,9 @@
 """
-Test script for scheduled_tasks/pilot_monitor
+Test script for scheduled_tasks/pilot_cleanup
 """
 
 import logging
-logger = logging.getLogger('scheduled_tasks_pilot_monitor_test')
+logger = logging.getLogger('scheduled_tasks_pilot_cleanup_test')
 
 import os
 import sys
@@ -11,6 +11,7 @@ import shutil
 import tempfile
 import unittest
 from functools import partial
+from datetime import datetime, timedelta
 from unittest.mock import patch, MagicMock
 
 from tornado.testing import AsyncTestCase
@@ -20,11 +21,12 @@ from rest_tools.client import RestClient
 from tests.util import unittest_reporter, glob_tests
 
 from iceprod.server.modules.schedule import schedule
-from iceprod.server.scheduled_tasks import pilot_monitor
+from iceprod.server.scheduled_tasks import pilot_cleanup
+from iceprod.server.util import datetime2str, nowstr
 
-class pilot_monitor_test(AsyncTestCase):
+class pilot_cleanup_test(AsyncTestCase):
     def setUp(self):
-        super(pilot_monitor_test,self).setUp()
+        super(pilot_cleanup_test,self).setUp()
         self.test_dir = tempfile.mkdtemp(dir=os.getcwd())
         def cleanup():
             shutil.rmtree(self.test_dir)
@@ -43,9 +45,9 @@ class pilot_monitor_test(AsyncTestCase):
         }
 
     @unittest_reporter
-    def test_100_pilot_monitor(self):
+    def test_100_pilot_cleanup(self):
         s = schedule(self.cfg,None,None,None)
-        pilot_monitor.pilot_monitor(s)
+        pilot_cleanup.pilot_cleanup(s)
 
     @unittest_reporter
     async def test_200_run(self):
@@ -53,23 +55,31 @@ class pilot_monitor_test(AsyncTestCase):
         pilots = {}
         async def client(method, url, args=None):
             logger.info('REST: %s, %s', method, url)
-            if url.startswith('/pilots'):
-                client.called = True
+            if method == 'GET' and url.startswith('/pilots'):
                 return pilots
+            elif method == 'DELETE' and url.startswith('/pilots'):
+                client.called = True
             else:
                 raise Exception()
         client.called = False
         rc.request = client
-        mon = MagicMock(spec=StatsClient)
 
-        await pilot_monitor.run(rc, mon, debug=True)
+        await pilot_cleanup.run(rc, debug=True)
+        self.assertFalse(client.called)
+
+        pilots['a'] = {'pilot_id':'a', 'grid_queue_id':'', 'last_update':nowstr()}
+        await pilot_cleanup.run(rc, debug=True)
         self.assertTrue(client.called)
-        self.assertTrue(mon.gauge.called)
 
-        mon.gauge.reset_mock()
-        pilots['a'] = {'tasks':[],'available':{'cpu':1,'time':10},'claimed':{}}
-        await pilot_monitor.run(rc, mon, debug=True)
-        self.assertTrue(mon.gauge.called)
+        client.called = False
+        pilots['a'] = {'pilot_id':'a', 'grid_queue_id':'blah', 'last_update':datetime2str(datetime.utcnow()-timedelta(days=30))}
+        await pilot_cleanup.run(rc, debug=True)
+        self.assertTrue(client.called)
+
+        client.called = False
+        pilots['a'] = {'pilot_id':'a', 'grid_queue_id':'blah', 'last_update':nowstr()}
+        await pilot_cleanup.run(rc, debug=True)
+        self.assertFalse(client.called)
 
     @unittest_reporter(name='run() - error')
     async def test_201_run(self):
@@ -78,16 +88,15 @@ class pilot_monitor_test(AsyncTestCase):
             logger.info('REST: %s, %s', method, url)
             raise Exception()
         rc.request = client
-        mon = MagicMock(spec=StatsClient)
         with self.assertRaises(Exception):
-            await pilot_monitor.run(rc, mon, debug=True)
+            await pilot_cleanup.run(rc, debug=True)
 
         # check it normally hides the error
-        await pilot_monitor.run(rc, mon, debug=False)
+        await pilot_cleanup.run(rc, debug=False)
 
 
 def load_tests(loader, tests, pattern):
     suite = unittest.TestSuite()
-    alltests = glob_tests(loader.getTestCaseNames(pilot_monitor_test))
-    suite.addTests(loader.loadTestsFromNames(alltests,pilot_monitor_test))
+    alltests = glob_tests(loader.getTestCaseNames(pilot_cleanup_test))
+    suite.addTests(loader.loadTestsFromNames(alltests,pilot_cleanup_test))
     return suite
