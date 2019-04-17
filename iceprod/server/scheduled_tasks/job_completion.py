b4803b50c79547e88ac5f235db1593d611295386
David Schultz <davids24@gmail.com>
2019-04-17 14:17:03 -0500

    run job completion on truncated datasets too

--- iceprod/server/scheduled_tasks/job_completion.py
+++ iceprod/server/scheduled_tasks/job_completion.py
@@ -37,30 +37,33 @@ async def run(rest_client, debug=False):
     datasets = await rest_client.request('GET', '/dataset_summaries/status')
     dataset_ids = []
     if 'processing' in datasets:
-        for dataset_id in datasets['processing']:
-            try:
-                jobs = await rest_client.request('GET', '/datasets/{}/job_summaries/status'.format(dataset_id))
-                if 'processing' not in jobs:
-                    continue
-                for job_id in jobs['processing']:
-                    tasks = await rest_client.request('GET', '/datasets/{}/tasks?keys=task_id|status&job_id={}'.format(dataset_id,job_id))
-                    task_statuses = set(t['status'] for t in tasks.values())
-                    if task_statuses == set(['complete']):
-                        logger.info('dataset %s job %s status -> complete', dataset_id, job_id)
-                        args = {'status':'complete'}
-                        await rest_client.request('PUT', '/datasets/{}/jobs/{}/status'.format(dataset_id,job_id), args)
-                    elif all(s not in task_statuses for s in ('idle','waiting','queued','processing','reset','resume')):
-                        if 'failed' in task_statuses:
-                            logger.info('dataset %s job %s status -> errors', dataset_id, job_id)
-                            args = {'status':'errors'}
-                        else:
-                            logger.info('dataset %s job %s status -> suspended', dataset_id, job_id)
-                            args = {'status':'suspended'}
-                        await rest_client.request('PUT', '/datasets/{}/jobs/{}/status'.format(dataset_id,job_id), args)
-            except Exception:
-                logger.error('error completing a job in dataset %s', dataset_id, exc_info=True)
-                if debug:
-                    raise
+        dataset_ids.extend(list(datasets['processing']))
+    if 'truncated' in datasets:
+        dataset_ids.extend(list(datasets['truncated']))
+    for dataset_id in dataset_ids:
+        try:
+            jobs = await rest_client.request('GET', '/datasets/{}/job_summaries/status'.format(dataset_id))
+            if 'processing' not in jobs:
+                continue
+            for job_id in jobs['processing']:
+                tasks = await rest_client.request('GET', '/datasets/{}/tasks?keys=task_id|status&job_id={}'.format(dataset_id,job_id))
+                task_statuses = set(t['status'] for t in tasks.values())
+                if task_statuses == set(['complete']):
+                    logger.info('dataset %s job %s status -> complete', dataset_id, job_id)
+                    args = {'status':'complete'}
+                    await rest_client.request('PUT', '/datasets/{}/jobs/{}/status'.format(dataset_id,job_id), args)
+                elif all(s not in task_statuses for s in ('idle','waiting','queued','processing','reset','resume')):
+                    if 'failed' in task_statuses:
+                        logger.info('dataset %s job %s status -> errors', dataset_id, job_id)
+                        args = {'status':'errors'}
+                    else:
+                        logger.info('dataset %s job %s status -> suspended', dataset_id, job_id)
+                        args = {'status':'suspended'}
+                    await rest_client.request('PUT', '/datasets/{}/jobs/{}/status'.format(dataset_id,job_id), args)
+        except Exception:
+            logger.error('error completing a job in dataset %s', dataset_id, exc_info=True)
+            if debug:
+                raise
 
     # run again after 60 minute delay
     stop_time = time.time()
