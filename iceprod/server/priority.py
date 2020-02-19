aa20058c85fd3c91cdf87c63b34bb164c0f48f76
David Schultz <davids24@gmail.com>
2020-02-19 15:26:37 -0600

    add new priority calculations for datasets and tasks

---
+++ iceprod/server/priority.py
@@ -0,0 +1,161 @@
+"""
+Functions used to calculate dataset and task priority.
+"""
+
+
+class Priority:
+    def __init__(self, rest_client):
+        self.rest_client = rest_client
+        self.dataset_cache = {}
+        self.user_cache = {}
+        self.group_cache = {}
+
+    async def _populate_dataset_cache(self):
+        args = {
+            'keys': 'dataset_id|priority|jobs_submitted|tasks_submitted|group|username',
+            'status': 'processing|truncated',
+        }
+        self.dataset_cache = await rest_client.request('GET', '/datasets', args)
+        for dataset_id in self.dataset_cache:
+            args = {
+                'keys': 'task_id|job_index|task_index',
+                'status': 'waiting|queued|processing|reset',
+            }
+            ret = await rest_client.request('GET', f'/datasets/{dataset_id}/tasks', args)
+            self.dataset_cache[dataset_id]['tasks'] = ret
+
+    async def _populate_user_cache(self):
+        ret = await rest_client.request('GET', '/users')
+        for user in ret['results']:
+            self.user_cache[user['username']] = user
+
+    async def _populate_group_cache(self):
+        ret = await rest_client.request('GET', '/groups')
+        for group in ret['results']:
+            self.user_cache[group['name']] = group
+
+    async def _get_dataset(self, dataset_id):
+        if not self.dataset_cache:
+            await self._populate_dataset_cache()
+        return self.dataset_cache[dataset_id]
+
+    async def _get_max_dataset_prio_user(self, user):
+        if not self.dataset_cache:
+            await self._populate_dataset_cache()
+        try:
+            return max(d['priority'] for d in self.dataset_cache.values() if d['username'] == user)
+        except ValueError:
+            return 1.
+
+    async def _get_user_prio(self, user):
+        if not self.user_cache:
+            await self._populate_user_cache()
+        return self.user_cache[user]['priority']
+
+    async def _get_group_prio(self, group):
+        if not self.group_cache:
+            await self._populate_group_cache()
+        return self.group_cache[group]['priority']
+
+    async def _get_max_user_prio_group(self, group):
+        if not self.group_cache:
+            await self._populate_group_cache()
+        try:
+            return max(g['priority'] for g in self.group_cache.values() if g['name'] == group)
+        except ValueError:
+            return 1.
+
+    async def _get_max_group_prio(self):
+        if not self.group_cache:
+            await self._populate_group_cache()
+        try:
+            return max(g['priority'] for g in self.group_cache.values())
+        except ValueError:
+            return 1
+
+    async def _get_num_tasks(self, dataset_id=None):
+        if not self.dataset_cache:
+            await self._populate_dataset_cache()
+        num = 0
+        for d in self.dataset_cache:
+            if dataset_id is None or dataset_id == d:
+                try:
+                    num += len(self.dataset_cache[d]['tasks'])
+                except ValueError:
+                    pass
+        return num
+
+    async def _get_dataset(self, dataset_id):
+        if not self.dataset_cache:
+            await self._populate_dataset_cache()
+        return self.dataset_cache[dataset_id]
+
+    async def get_dataset_prio(self, dataset_id):
+        """
+        Calculate priority for a dataset.
+
+        Args:
+            dataset_id: dataset id
+
+        Returns:
+            float: priority between 0 and 1
+        """
+        dataset = await self._get_dataset(dataset_id)
+        dataset_prio = dataset['priority']
+        user = dataset['username']
+        group = dataset['group']
+        max_dataset_prio = await self._get_max_dataset_prio_user(user)
+
+        user_prio = await self._get_user_prio(user)
+        max_user_prio = await self._get_max_user_prio_group(group)
+
+        group_prio = await self._get_group_prio(group)
+        max_group_prio = await self._get_max_group_prio()
+
+        num_all_tasks = await self._get_num_tasks()
+        num_dataset_tasks = await self._get_num_tasks(dataset_id)
+
+        # general priority
+        priority = 1. * dataset_prio / max_dataset_prio * user_prio / max_user_prio * group_prio / max_group_prio
+
+        # bias against large datasets
+        priority -= (1. - 1. * (num_all_tasks - num_dataset_tasks) / num_all_tasks) / 3.
+
+        if priority < 0.:
+            priority = 0.
+        elif priority > 1.:
+            priority = 1.
+
+        return priority
+
+    async def get_task_prio(self, dataset_id, task_id):
+        """
+        Calculate priority for a task.
+
+        Args:
+            dataset_id: dataset id
+            task_id: task id
+
+        Returns:
+            float: priority between 0 and 1
+        """
+        priority = await self.get_dataset_prio(dataset_id)
+
+        dataset = await self._get_dataset(dataset_id)
+        task = dataset['tasks'][task_id]
+
+        tasks_per_job = dataset['jobs_submitted'] / dataset['tasks_submitted']
+
+        # bias towards finishing jobs
+        priority += (1. * task['task_index'] / tasks_per_job) / 10.
+
+        # bias towards first 100 jobs (or small datasets)
+        if task['job_index'] < 100:
+            priority += (100. - task['job_index']) / 100.
+
+        if priority < 0.:
+            priority = 0.
+        elif priority > 1.:
+            priority = 1.
+
+        return priority
---
+++ tests/server/priority_test.py
@@ -0,0 +1,112 @@
+"""
+Test script for priority
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from tests.util import unittest_reporter, glob_tests
+
+import logging
+logger = logging.getLogger('priority_test')
+
+import os, sys, time
+import shutil
+import tempfile
+import random
+import unittest
+
+import iceprod.server
+from iceprod.server import priority
+
+def prio_setup():
+    datasets = {
+        'd0': {
+            'dataset_id': 'd0',
+            'jobs_submitted': 10,
+            'tasks_submitted': 20,
+            'priority': 1,
+            'group': 'g_a',
+            'username': 'u_a',
+            'tasks': {
+                't0': {'task_id': 't0', 'task_index': 0, 'job_index': 0},
+                't1': {'task_id': 't1', 'task_index': 1, 'job_index': 0},
+            }
+        },
+        'd1': {
+            'dataset_id': 'd1',
+            'jobs_submitted': 10,
+            'tasks_submitted': 20,
+            'priority': 1,
+            'group': 'g_a',
+            'username': 'u_b',
+            'tasks': {
+                't2': {'task_id': 't2', 'task_index': 0, 'job_index': 0},
+                't3': {'task_id': 't3', 'task_index': 1, 'job_index': 0},
+            }
+        },
+        'd2': {
+            'dataset_id': 'd2',
+            'jobs_submitted': 1000,
+            'tasks_submitted': 20000,
+            'priority': 1,
+            'group': 'g_a',
+            'username': 'u_a',
+            'tasks': {
+                't4': {'task_id': 't4', 'task_index': 0, 'job_index': 400},
+                't5': {'task_id': 't5', 'task_index': 1, 'job_index': 400},
+            }
+        },
+    }
+    users = {
+        'u_a': {'username': 'u_a', 'priority': 1.},
+        'u_b': {'username': 'u_b', 'priority': .5},
+    }
+    groups = {
+        'g_a': {'name':'g_a', 'priority': 1.},
+    }
+
+    p = priority.Priority(None)
+    p.dataset_cache = datasets
+    p.user_cache = users
+    p.group_cache = groups
+    return p
+
+class priority_test(unittest.TestCase):
+    def setUp(self):
+        super(priority_test,self).setUp()
+        self.test_dir = tempfile.mkdtemp(dir=os.getcwd())
+        def cleanup():
+            shutil.rmtree(self.test_dir)
+        self.addCleanup(cleanup)
+
+    @unittest_reporter
+    async def test_10_get_dataset_prio(self):
+        """Test get_dataset_prio"""
+        p = prio_setup()
+        prio1 = await p.get_dataset_prio('d0')
+        prio2 = await p.get_dataset_prio('d1')
+        self.assertLess(prio2, prio1)
+
+        prio3 = await p.get_dataset_prio('d2')
+        self.assertEqual(prio3, prio1)
+
+    @unittest_reporter
+    async def test_20_get_task_prio(self):
+        """Test get_task_prio"""
+        p = prio_setup()
+        prio1 = await p.get_task_prio('d0', 't0')
+        prio2 = await p.get_task_prio('d1', 't2')
+        # equal at 1.0 because of boost
+        self.assertEqual(prio2, prio1)
+
+        prio3 = await p.get_task_prio('d2', 't4')
+        prio4 = await p.get_task_prio('d2', 't5')
+        self.assertLess(prio3, prio1) # 
+        self.assertLess(prio3, prio4) # ordering of tasks in a job
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    alltests = glob_tests(loader.getTestCaseNames(priority_test))
+    suite.addTests(loader.loadTestsFromNames(alltests,priority_test))
+    return suite
