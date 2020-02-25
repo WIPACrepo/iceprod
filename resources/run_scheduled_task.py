084a7558f1bd188692b07b2536cc41e40247e7d7
David Schultz <davids24@gmail.com>
2020-02-25 14:07:36 -0600

    run any scheduled task on demand

---
+++ resources/run_scheduled_task.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import sys
+import argparse
+import logging
+import asyncio
+import importlib
+
+from rest_tools.client import RestClient
+
+iceprod_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,iceprod_path)
+
+logger = logging.getLogger()
+
+def main():
+    parser = argparse.ArgumentParser(description='run a scheduled task once')
+    parser.add_argument('-t', '--token', help='auth token')
+    parser.add_argument('--debug', default=False, type=bool, help='debug enabled/disabled')
+    parser.add_argument('scheduled_task', help='name of the scheduled task to run')
+
+    args = parser.parse_args()
+    args = vars(args)
+
+    logformat='%(asctime)s %(levelname)s %(name)s %(module)s:%(lineno)s - %(message)s'
+    logging.basicConfig(format=logformat, level=logging.DEBUG if args['debug'] else logging.INFO)
+    
+    rpc = RestClient('https://iceprod2-api.icecube.wisc.edu', args['token'])
+
+    mod = importlib.import_module('iceprod.server.scheduled_tasks.'+args['scheduled_task'])
+    fn = getattr(mod,'run')
+
+    asyncio.run(fn(rpc, debug=args['debug']))
+
+if __name__ == '__main__':
+    main()
