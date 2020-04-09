f79b33c9a069616c974b24dc85684a357fd50633
David Schultz <davids24@gmail.com>
2020-04-08 19:49:07 -0500

    adjust priority of tasks with dependencies so they don't keep trying to queue

--- iceprod/server/scheduled_tasks/queue_tasks.py
+++ iceprod/server/scheduled_tasks/queue_tasks.py
@@ -11,6 +11,7 @@ Periodic delay: 5 minutes
 import logging
 import random
 import time
+import asyncio
 from collections import defaultdict
 
 from tornado.ioloop import IOLoop
@@ -50,42 +51,56 @@ async def run(rest_client, debug=False):
         if 'queued' in tasks:
             num_tasks_queued = tasks['queued']
         tasks_to_queue = min(num_tasks_waiting, NTASKS - num_tasks_queued, NTASKS_PER_CYCLE)
-        logger.info(f'num tasks waiting: {num_tasks_waiting}')
-        logger.info(f'num tasks queued: {num_tasks_queued}')
-        logger.info(f'tasks to queue: {tasks_to_queue}')
+        logger.warning(f'num tasks waiting: {num_tasks_waiting}')
+        logger.warning(f'num tasks queued: {num_tasks_queued}')
+        logger.warning(f'tasks to queue: {tasks_to_queue}')
 
         if tasks_to_queue > 0:
             args = {
                 'status': 'waiting',
                 'keys': 'task_id|depends',
                 'sort': 'priority=-1',
+                'limit': 5*tasks_to_queue,
             }
             ret = await rest_client.request('GET', '/tasks', args)
             queue_tasks = []
-            for task in ret['tasks']:
+            deps_futures = set()
+            async def check_deps(task):
                 for dep in task['depends']:
                     ret = await rest_client.request('GET', f'/tasks/{dep}')
                     if ret['status'] != 'complete':
-                        logger.debug('dependency not met for task %s', task['task_id'])
-                        break
-                else:
+                        logger.info('dependency not met for task %s', task['task_id'])
+                        await rest_client.request('PATCH', f'/tasks/{task["task_id"]}', {'priority': 0})
+                        return None
+                return task
+            for task in ret['tasks']:
+                if not task['depends']:
+                    logger.info('queueing task %s', task['task_id'])
                     queue_tasks.append(task['task_id'])
-                    if len(queue_tasks) >= tasks_to_queue:
-                        break
+                else:
+                    fut = asyncio.create_task(check_deps(task))
+                    deps_futures.add(fut)
+                    if len(deps_futures) >= 20:
+                        done, pending = await asyncio.wait(deps_futures, return_when=asyncio.FIRST_COMPLETED)
+                        for fut in done:
+                            task = await fut
+                            if task and len(queue_tasks) < tasks_to_queue:
+                                logger.info('queueing task %s', task['task_id'])
+                                queue_tasks.append(task['task_id'])
+                        deps_futures = pending
+                if len(queue_tasks) >= tasks_to_queue:
+                    break
 
-            logger.info('queueing %d tasks', len(queue_tasks))
+            for fut in asyncio.as_completed(deps_futures):
+                task = await fut
+                if task and len(queue_tasks) < tasks_to_queue:
+                    logger.info('queueing task %s', task['task_id'])
+                    queue_tasks.append(task['task_id'])
+
+            logger.warning('queueing %d tasks', len(queue_tasks))
             if queue_tasks:
                 args = {'tasks': queue_tasks}
                 await rest_client.request('POST', '/task_actions/bulk_status/queued', args)
-
-        # ~ while tasks_to_queue > 0:
-            # ~ num = min(tasks_to_queue, 10)
-            # ~ tasks_to_queue -= num
-
-            # ~ ret = await rest_client.request('POST', '/task_actions/queue', {'num_tasks': num})
-            # ~ logger.info(f'num queued: {ret["queued"]}')
-            # ~ if ret['queued'] < num:
-                # ~ break
 
     except Exception:
         logger.error('error queueing tasks', exc_info=True)
