f7f4aff59b795ea803bd3a918b4e7b5051eec83f
David Schultz <davids24@gmail.com>
2020-08-21 17:50:22 -0500

    fix job actions

--- iceprod/server/data/www/rest.js
+++ iceprod/server/data/www/rest.js
@@ -198,7 +198,7 @@ async function set_jobs_status(dataset_id, job_ids, stat, passkey, task_status_f
             if (messaging) {
                 message("updating tasks");
             }
-            return set_tasks_status(dataset_id, task_ids, task_status, passkey, false);
+            return await set_tasks_status(dataset_id, task_ids, task_status, passkey, false);
         }
         if (messaging) {
             message_close();
