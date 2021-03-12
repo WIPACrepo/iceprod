7aa68e1b11c83fd51583dc17fe5156dff70e3248
Ric Evans <emejqz@gmail.com>
2021-03-12 13:44:17 -0600

    autobuild `version_info` tuple

--- iceprod/__init__.py
+++ iceprod/__init__.py
@@ -9,5 +9,10 @@ from __future__ import absolute_import, division, print_function
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-__version__ = '2.5.5'
-version_info = (2, 5, 5, 0)
+__version__ = "2.5.5"
+version_info = (
+    int(__version__.split(".")[0]),
+    int(__version__.split(".")[1]),
+    int(__version__.split(".")[2]),
+    0,
+)
