4c9113ff034541251b4670152d776fa2f5e27d48
David Schultz <david.schultz@icecube.wisc.edu>
2023-08-15 15:52:10 -0500

    remove get_pkgdata_filename in favor of importlib.resources.files

--- iceprod/server/__init__.py
+++ iceprod/server/__init__.py
@@ -172,23 +172,6 @@ class KwargConfig(object):
                     os.makedirs(d)
 
 
-def get_pkgdata_filename(package, resource):
-    """Get a filename for a resource bundled within the package"""
-    loader = get_loader(package)
-    if loader is None or not hasattr(loader, 'get_data'):
-        return None
-    mod = sys.modules.get(package) or loader.load_module(package)
-    if mod is None or not hasattr(mod, '__file__'):
-        return None
-
-    # Modify the resource name to be compatible with the loader.get_data
-    # signature - an os.path format "filename" starting with the dirname of
-    # the package's __file__
-    parts = resource.split('/')
-    parts.insert(0, os.path.dirname(mod.__file__))
-    return os.path.join(*parts)
-
-
 def get_pkg_binary(package, binary):
     """Try finding the binary path based on the python package"""
     try:
