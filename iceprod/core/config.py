1c766183ada123b98224289efd2e2c6407b634cc
David Schultz <david.schultz@icecube.wisc.edu>
2023-08-18 15:35:52 -0500

    more config and tests

--- iceprod/core/config.py
+++ iceprod/core/config.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass
 import importlib.resources
 import json
@@ -15,6 +16,7 @@ CONFIG_SCHEMA = json.loads((importlib.resources.files('iceprod.core')/'data'/'da
 
 @dataclass
 class Dataset:
+    """IceProd Dataset config and basic attributes"""
     dataset_id: str
     dataset_num: int
     group: str
@@ -23,7 +25,7 @@ class Dataset:
 
     @classmethod
     async def load_from_api(cls, dataset_id: str, rest_client: RestClient) -> Self:
-        dataset = await rest_client.request('GET', f'/dataset/{dataset_id}')
+        dataset = await rest_client.request('GET', f'/datasets/{dataset_id}')
         config = await rest_client.request('GET', f'/config/{dataset_id}')
         return cls(dataset_id, dataset['dataset'], dataset['group'], dataset['username'], config)
 
@@ -50,4 +52,52 @@ class Dataset:
     def validate(self):
         jsonschema.validate(self.config, CONFIG_SCHEMA)
 
-        
+
+@dataclass
+class Job:
+    """IceProd Job instance"""
+    dataset: Dataset
+    job_id: str
+    job_index: int
+    status: str
+
+
+@dataclass
+class Task:
+    """
+    IceProd Task instance, ready for running.
+
+    Old task stats are not loaded by default, but can be loaded on request.
+    """
+    dataset: Dataset
+    job: Job
+    task_id: str
+    task_index: int
+    name: str
+    depends: list
+    requirements: dict
+    status: str
+    site: str
+    stats: dict
+
+    @classmethod
+    async def load_from_api(cls, dataset_id: str, task_id: str, rest_client: RestClient) -> Self:
+        dataset, config, task = await asyncio.gather(
+            rest_client.request('GET', f'/datasets/{dataset_id}'),
+            rest_client.request('GET', f'/config/{dataset_id}'),
+            rest_client.request('GET', f'/datasets/{dataset_id}/tasks/{task_id}')
+        )
+        d = Dataset(dataset_id, dataset['dataset'], dataset['group'], dataset['username'], config)
+        job = await rest_client.request('GET', f'/datasets/{dataset_id}/jobs/{task["job_id"]}')
+        j = Job(d, task['job_id'], job['job_index'], job['status'])
+        return cls(d, j, task['task_id'], task['task_index'], task['name'], task['depends'], task['requirements'], task['status'], task['site'], {})
+
+    async def load_stats_from_api(self, rest_client: RestClient):
+        ret = await rest_client.request('GET', f'/datasets/{self.dataset.dataset_id}/tasks/{self.task_id}/task_stats', {'last': 'true'})
+        if not ret:
+            raise Exception('No stats to load!')
+        # get first (only) result in ret
+        self.stats = next(iter(ret.values()))
+
+    def get_task_config(self):
+        return self.dataset.config['tasks'][self.task_index]
