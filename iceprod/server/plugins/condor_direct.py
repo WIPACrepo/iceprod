efa52acf9aafea60ac974d236363bddb4c3397e5
David Schultz <davids24@gmail.com>
2023-09-13 13:48:01 -0500

    Fix os selection (#361)
    * fix OS selection to prefer OSG string if present
    
    * python not found is a node error
    
    * <bot> update requirements-docs.txt
    
    * <bot> update requirements-tests.txt
    
    * <bot> update requirements.txt
    
    * update to latest rest tools syntax
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- iceprod/server/plugins/condor_direct.py
+++ iceprod/server/plugins/condor_direct.py
@@ -205,6 +205,7 @@ class condor_direct(grid.BaseGrid):
                         or 'No space left on device' in line
                         or 'Illegal instruction' in line
                         or ('Killed' in line and 'env-shell.sh' in line)
+                        or ('python: command not found' in line and 'env-shell.sh' in line)
                         or 'py3-v4.1.1/RHEL_8_x86_64/lib/libCore.so.6.18: undefined symbol: usedToIdentifyRootClingByDlSym' in line):
                     payload_failure = False
                     break
