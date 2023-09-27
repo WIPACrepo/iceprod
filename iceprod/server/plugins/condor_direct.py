d939a447436152fd35badf55fcc33c05f455c66a
David Schultz <davids24@gmail.com>
2023-09-27 15:20:29 -0500

    mark cvmfs client errors as node errors (#364)
    * mark cvmfs client errors as node errors
    
    * <bot> update requirements-docs.txt
    
    * <bot> update requirements-tests.txt
    
    * <bot> update requirements.txt
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- iceprod/server/plugins/condor_direct.py
+++ iceprod/server/plugins/condor_direct.py
@@ -204,6 +204,7 @@ class condor_direct(grid.BaseGrid):
                 if ('No such file or directory' in line
                         or 'No space left on device' in line
                         or 'Illegal instruction' in line
+                        or 'Input/output error' in line
                         or ('Killed' in line and 'env-shell.sh' in line)
                         or ('python: command not found' in line and 'env-shell.sh' in line)
                         or 'py3-v4.1.1/RHEL_8_x86_64/lib/libCore.so.6.18: undefined symbol: usedToIdentifyRootClingByDlSym' in line
