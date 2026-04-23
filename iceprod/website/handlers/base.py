ae506f21a274c565772f4d755081fa42212bddf7
David Schultz <david.schultz@icecube.wisc.edu>
2026-04-23 14:47:47 -0500

    actually add the rest api to the right place in the csp

--- iceprod/website/handlers/base.py
+++ iceprod/website/handlers/base.py
@@ -407,9 +407,9 @@ class PublicHandler(LoginMixin, TokenStorageMixin, PromRequestMixin, RestHandler
         self.csp_nonce = secrets.token_urlsafe(16)
         csp_policy = (
             "default-src 'none'; "
-            f"script-src 'self' 'nonce-{self.csp_nonce}' {self.rest_api} cdnjs.cloudflare.com; "
+            f"script-src 'self' 'nonce-{self.csp_nonce}' cdnjs.cloudflare.com; "
             "script-src-attr 'unsafe-inline'; "
-            "connect-src 'self'; "
+            f"connect-src 'self' {self.rest_api}; "
             "img-src 'self'; "
             f"style-src 'self' 'nonce-{self.csp_nonce}' cdnjs.cloudflare.com; "
             "style-src-attr 'unsafe-inline'; "
